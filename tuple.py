9ece769ca32fa713b907a7035afee86e94ec3630
Rijo007 <rijovarghese225@gmail.com>
2026-02-27 23:53:35 +0530

    learned tuple

---
+++ tuple.py
@@ -0,0 +1,30 @@
+# # we can give any data tpe in tuple , string also usinh ""
+
+# num=(10,20,30,7.5,"string",True,False)   
+
+# # the abouve is not tuple , atleat we need write two  elements or after , on  tuple
+
+# # num1=(10) 
+
+# num2=(10,)
+
+
+# #slicing of tuple
+
+# a=(10,20,30,40,"rijo")
+
+
+# num[0]=50
+
+
+# print(a)
+
+
+
+#tuple method
+
+num=(2,11,10,30)
+
+print(num.index(10)) # position in 
+
+print(num.count(10)) # counting
