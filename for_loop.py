aded510602bf70af05d03b8251b8960ebc3e57be
Rijo007 <rijovarghese225@gmail.com>
2026-03-03 00:20:30 +0530

    forloop

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,42 @@
-#starting value ,ending value, step sizing(initializing,condition,incriment)
+starting value ,ending value, step sizing(initializing,condition,incriment)
 for i in range(1,10,1):#initial value, range(condition),incriment, but here only print 9 values. if i waana print , put 11 range
     
     print(i)
     print("hello")
+    
+    accessing each strings in using for loop
+    
+s="welcome"
+
+for i in s:
+    print(i)
+
+l=[1,2,3,4]
+
+for i in l:
+    print(i)
+
+
+lst=[10,11,12,13,14,15]
+
+for i in lst:
+    if i%2==0:
+        
+        print(i)
+
+
+sum=0
+
+for i in range(1,101,1):
+    sum=sum+i
+    
+print(sum)
+
+    
+#multiplication 
+
+num=(int(input("enter the number:")))   
+    
+for i in range(1,11,1):
+    
+    print(i,"*",num,"=",i*num)
