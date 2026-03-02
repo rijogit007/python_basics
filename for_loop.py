cd3965eaa7cd56a6092a832fec705f486245b1b9
Rijo007 <rijovarghese225@gmail.com>
2026-03-02 23:50:38 +0530

    forloop

---
+++ for_loop.py
@@ -0,0 +1,5 @@
+#starting value ,ending value, step sizing(initializing,condition,incriment)
+for i in range(1,10,1):#initial value, range(condition),incriment, but here only print 9 values. if i waana print , put 11 range
+    
+    print(i)
+    print("hello")
