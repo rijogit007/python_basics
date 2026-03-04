f3f481c1e36f99a78fc90c13bd2a2f26e41ff3c2
Rijo007 <rijovarghese225@gmail.com>
2026-03-04 23:50:06 +0530

    whileloop1

--- while.py
+++ while.py
@@ -1,3 +1,28 @@
-i=1
-while i<=10:
-    print(i)
+# i=1
+# while True:
+#     print(i)
+    
+#     i=i+1
+#     # break
+#     continue
+
+
+#     print("hello")
+
+
+
+x=10
+
+while True:
+    n=int(input("enter the number"))
+    
+    if n<x:
+        print("value is small plz enter correct number")
+        
+    elif n>x:
+        
+        print("value is high plz enter correct number")
+        
+    else:
+        print("value is correct")            
+        break
