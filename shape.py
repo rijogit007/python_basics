e8af052b59dd37410e40c8e9ed15cab0be781ec5
Rijo007 <rijovarghese225@gmail.com>
2026-03-06 08:18:03 +0530

    new commit

---
+++ shape.py
@@ -0,0 +1,24 @@
+# num=1
+# for row in range(3):
+#     for col in range(3):
+#         print(num,end=" ")
+#         num=num+1
+#     print()    
+
+# num=0
+# for row in range(5):
+#     num=num+1
+#     for col in range(5):
+#         print(num,end=" ")
+        
+#     print()    
+
+
+col=1
+for row in range(5):
+   
+    for col in range(1,6):
+        
+        print(col,end=" ")
+        
+    print()   
