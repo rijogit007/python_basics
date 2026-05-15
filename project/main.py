c82177ec4767885a371cce493b7e8f064b6196d6
Rijo007 <rijovarghese225@gmail.com>
2026-05-15 08:34:26 +0530

    first commit

--- project/main.py
+++ project/main.py
@@ -3,8 +3,8 @@ def deposit():
         
         myamount=input("what would u like to deposit")
         if myamount.isdigit():
-            # myamount=int(myamount)
-            # if myamount>0:
-            #     break
+            myamount=int(myamount)
+            if myamount>0:
+                break
             # else:
             #     print("Amount must graeter than zero")
